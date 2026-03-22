053e424a7cdc19e0c1bba1382d01c8f10c5ce90b
monikavalkova <66033306+monikavalkova@users.noreply.github.com>
2026-03-22 12:44:02 +0100

    Downgrade two dependencies to test   │ │ (#4)
    │ │ Dependabot                                                                                                                                                                                            │ │
     │ │                                                                                                                                                                                                       │ │
     │ │ - Downgrade androidx.core:core-ktx from 1.15.0 to 1.13.0                                                                                                                                              │ │
     │ │ - Downgrade androidx.lifecycle:lifecycle-runtime-ktx from 2.8.7 to 2.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,8 +42,8 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.compose.ui:ui")
